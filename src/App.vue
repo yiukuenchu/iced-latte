ccd7430d6ce4d314cc510cf52b9c9b64311e24f2
yiukuenchu <yiukuenchu@gmail.com>
2019-08-29 17:40:08 +0800

    add medium card

--- src/App.vue
+++ src/App.vue
@@ -25,13 +25,13 @@
           icon="sspai.png"
           link="https://beta.sspai.com/u/i668zmn5/posts"
         />
-        <!-- <Card
-          msg="Medium tech blogs"
+        <Card
+          msg="Blogs on Medium"
           backgroundColor="#1AA77D"
           textColor="#FFFFFF"
           icon="medium.png"
-          link="https://medium.com/spencerweekly"
-        /> -->
+          link="https://medium.com/coffee-minutes"
+        />
       </div>
 
       <div id="others">
